b7b76e1f0ea0ba0103e656561ce264f30378fd2a
zidanhost1 <96322785+zidanhost1@users.noreply.github.com>
2024-10-27 18:35:57 +0700

    Update main.js

--- baymax/main.js
+++ baymax/main.js
@@ -1,25 +1,31 @@
+function randomTime(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function berkedip() {	
+	const kedip = randomTime(1,3);
+	const eyes = document.getElementsByClassName("eyes")
+
+	// console.log(kedip)
+    eyes[0].style.animation = 'none'; // Reset the animation
+    eyes[1].style.animation = 'none'; // Reset the animation
+
+    // Trigger reflow to reset animation
+    void eyes[0].offsetWidth;
+
+    // Set the animation again
+    eyes[0].style.animation = `eyes .30s ease 0.1s ${kedip}`;
+    eyes[1].style.animation = `eyes .30s ease 0.1s ${kedip}`;
+}
+
+
+
+
 window.addEventListener("load",()=>{
 	console.log("LOADED")
 	const face = document.getElementById("face")
-	const eyes = document.getElementsByClassName("eyes")
 
+	face.addEventListener("click", berkedip);
+	setInterval(berkedip, 20000)
 
-	function berkedip(min, max) {
-	    return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
-
-
-	face.addEventListener("click", function() {	
-		let kedip = berkedip(1,2);
-		console.log(kedip)
-        eyes[0].style.animation = 'none'; // Reset the animation
-        eyes[1].style.animation = 'none'; // Reset the animation
-
-        // Trigger reflow to reset animation
-        void eyes[0].offsetWidth;
-
-        // Set the animation again
-        eyes[0].style.animation = `eyes 1s ease 0s ${kedip}`;
-        eyes[1].style.animation = `eyes 1s ease 0s ${kedip}`;
-    });
 })
